c3d3b09458a2ff05e7346866bb44be6abd90ed69
jayy-codes07 <hadiyajay2010@gmail.com>
2026-03-23 15:30:28 +0530

    add user registration route and async error handling

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,7 +1,10 @@
-const asyncHandler = (fn) => async () => {
+const asyncHandler = (fn) => async (req, res, next) => {
   try {
     await fn(req, res, next);
-  } catch (error) {
+  } catch (err) {
     res.status(err.code || 500).json({ success: false, message: err.message });
   }
 };
+
+
+export {asyncHandler}
