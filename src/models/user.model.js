d7fdaf63e1520fd969b97bd08a25f90035bf3bb8
jayy-codes07 <hadiyajay2010@gmail.com>
2026-03-23 11:54:07 +0530

    add Cloudinary and Multer , implement file upload  functionality

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -53,7 +53,6 @@ const userSchema = mongoose.Schema(
 
 userSchema.pre("save", async function (next) {
   if (!this.isModified("password")) return next();
-
   this.password = bcrypt.hash(this.password, 10);
   next();
 });
