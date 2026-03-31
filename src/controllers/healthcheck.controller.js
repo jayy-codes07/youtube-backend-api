153b754692e17176af03c94151d1a325e4e47076
jayy-codes07 <hadiyajay2010@gmail.com>
2026-03-31 17:21:01 +0530

    add controllers and routes for comments, dashboard, healthcheck, likes, playlists, subscriptions, tweets, and videos

---
+++ src/controllers/healthcheck.controller.js
@@ -0,0 +1,14 @@
+import { ApiError } from "../utils/ApiError.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
+
+const healthcheck = asyncHandler(async (req, res) => {
+  if (!req) {
+    res.ApiError(400, "there is problem in api");
+  }
+
+  res.status(200).json(ApiResponse(200, {}, "api running perfectly fine"));
+  //TODO: build a healthcheck response that simply returns the OK status as json with a message
+});
+
+export { healthcheck };
