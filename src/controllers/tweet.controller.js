1f3be39d3e410f6ef47ccc21c68f8d1a391eeb2d
jayy-codes07 <hadiyajay2010@gmail.com>
2026-04-11 11:11:02 +0530

    in tweet controller  proper validation for tweet creation, updating, and deletion

--- src/controllers/tweet.controller.js
+++ src/controllers/tweet.controller.js
@@ -19,22 +19,64 @@ const createTweet = asyncHandler(async (req, res) => {
         throw new ApiError(500, "there is problem in creating tweet")
     }
 
-    res.status(200).json(new ApiResponse(200, tweet, "tweet created successfully"))
+    res.status(201).json(new ApiResponse(201, tweet, "tweet created successfully"))
 
 })
 
 
 
 const getUserTweets = asyncHandler(async (req, res) => {
+    const { userId } = req.params;
     // TODO: get user tweets
+    const userAllComment = await Tweet.find({ owner: userId })
+
+    if (!userAllComment?.length) {
+        return res.status(200).json(new ApiResponse(200, [], "there is not tweets there "))
+        
+    }
+
+    res.status(200).json(new ApiResponse(200, userAllComment, "here are all users"))
 })
 
 const updateTweet = asyncHandler(async (req, res) => {
+
+    const { content } = req.body;
+    const { tweetId } = req.params;
+
+    if (!tweetId?.trim()) {
+        throw new ApiError(400, "provide proper tweetId")
+    }
+
+    if (!content?.trim()) {
+        throw new ApiError(400, "there is no content available there ")
+    }
+
+
+    const updatedtweet = await Tweet.findOneAndUpdate({ _id: tweetId, owner: req.user._id }, { $set: { content: content?.trim() } }, { new: true })
+
+    if (!updatedtweet) {
+        throw new ApiError(500, "there is problem in updating tweet")
+    }
+
+    res.status(200).json(new ApiResponse(200, updatedtweet, "tweet update successfully"))
     //TODO: update tweet
 })
 
 const deleteTweet = asyncHandler(async (req, res) => {
     //TODO: delete tweet
+    const { tweetId } = req.params;
+    if (!tweetId?.trim()) {
+        throw new ApiError(400, "provide proper tweetId")
+    }
+
+
+    const tweetdelete = await Tweet.findOneAndDelete({ _id: tweetId, owner: req.user._id })
+
+    if (!tweetdelete) {
+        throw new ApiError(500, "proble in deleting tweet")
+    }
+
+    res.status(200).json(new ApiResponse(200, tweetdelete, "tweet deleted sucessfully"))
 })
 
 export {
